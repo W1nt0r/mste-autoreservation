68f3aba19cfa60bdf8b3788fc5bb6ad4587aa02a
Gabriel Vlasek <gvlasek@bluewin.ch>
2017-11-23 15:53:13 +0100

    Added Kunde & Reservation

--- AutoReservation.Dal/Entities/Kunde.cs
+++ AutoReservation.Dal/Entities/Kunde.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 
 namespace AutoReservation.Dal.Entities
 {
-  
+    public class Kunde
+    {
+        public DateTime Geburtsdatum { get; set; }
+        public int Id { get; set; }
+        public String Nachname { get; set; }
+        public byte[] RowVersion { get; set; }
+        public String Vorname { get; set; }
+        public virtual List<Reservation> Reservations { get; set; }
+    }
 }
--- AutoReservation.Dal/Entities/Reservation.cs
+++ AutoReservation.Dal/Entities/Reservation.cs
@@ -4,5 +4,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoReservation.Dal.Entities
 {
-    
+    public class Reservation
+    { 
+        DateTime Bis { get; set; }
+        DateTime Von { get; set; }
+        public int ReservationNr { get; set; }
+        public byte[] RowVersion { get; set; }
+
+        public int AutoId { get; set; }
+        [ForeignKey("AutoId")]
+        public Auto Auto { get; set; }
+
+        public int KundeId { get; set; }
+        [ForeignKey("KundeId")]
+        public Kunde Kunde { get; set; }
+
+    }
 }
