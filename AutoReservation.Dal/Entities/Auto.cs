36d40306ff0f4fed32df2e30f4c442c78c4fa7c2
Supernaibaf <reiem_naibaf@hotmail.com>
2017-11-23 15:52:23 +0100

    Added Class Auto

--- AutoReservation.Dal/Entities/Auto.cs
+++ AutoReservation.Dal/Entities/Auto.cs
@@ -1,8 +1,25 @@
+using System.Collections;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 
 namespace AutoReservation.Dal.Entities
 {
- 
+    public abstract class Auto
+    {
+        public int Id { get; set; }
+        public string Marke { get; set; }
+        public byte[] RowVersion { get; set; }
+        public int Tagestarif { get; set; }
+        public ICollection<Reservation> Reservationen { get; set; }
+    }
+
+    public class StandardAuto : Auto { }
+
+    public class LuxusklasseAuto : Auto
+    {
+        public int Basistarif { get; set; }
+    }
+
+    public class MittelklasseAuto : Auto { }
 }
