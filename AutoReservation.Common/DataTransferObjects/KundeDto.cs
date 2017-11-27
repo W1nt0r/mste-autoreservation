a27f4430a0f319b5003f750240ad73e28221f1e9
w1nt0r <j4n.winter@gmail.com>
2017-11-27 18:03:29 +0100

    added dto implementation

--- AutoReservation.Common/DataTransferObjects/KundeDto.cs
+++ AutoReservation.Common/DataTransferObjects/KundeDto.cs
@@ -5,6 +5,11 @@ namespace AutoReservation.Common.DataTransferObjects
 {
     public class KundeDto
     {
+        public int Id { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+        public String Nachname { get; set; }
+        public String Vorname { get; set; }
+        public byte[] RowVersion { get; set; }
 
         //public override string ToString()
         //    => $"{Id}; {Nachname}; {Vorname}; {Geburtsdatum}; {RowVersion}";
--- AutoReservation.Common/DataTransferObjects/ReservationDto.cs
+++ AutoReservation.Common/DataTransferObjects/ReservationDto.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Runtime.Serialization;
+using AutoReservation.Dal.Entities;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
     public class ReservationDto
     {
+        public int ReservationNr { get; set; }
+        DateTime Bis { get; set; }
+        DateTime Von { get; set; }
+        public byte[] RowVersion { get; set; }
+
+        public Kunde Kunde { get; set; }
+        public Auto Auto { get; set; }
 
         //public override string ToString()
         //    => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
