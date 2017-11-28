7a70c9ef1f7003430faf9f005bfea7768573fc1d
w1nt0r <j4n.winter@gmail.com>
2017-11-28 09:13:59 +0100

    fixed reservationdto typos + refs

--- AutoReservation.Common/DataTransferObjects/ReservationDto.cs
+++ AutoReservation.Common/DataTransferObjects/ReservationDto.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Runtime.Serialization;
-using AutoReservation.Dal.Entities;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
     public class ReservationDto
     {
-        public int ReservationNr { get; set; }
-        DateTime Bis { get; set; }
-        DateTime Von { get; set; }
+        public int ReservationsNr { get; set; }
+        public DateTime Bis { get; set; }
+        public DateTime Von { get; set; }
         public byte[] RowVersion { get; set; }
 
-        public Kunde Kunde { get; set; }
-        public Auto Auto { get; set; }
+        public KundeDto Kunde { get; set; }
+        public AutoDto Auto { get; set; }
 
         //public override string ToString()
         //    => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
