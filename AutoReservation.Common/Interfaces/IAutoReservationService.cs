7f84b28afda2c0abceb88b7d8e7592dc0e1ddcad
w1nt0r <j4n.winter@gmail.com>
2017-12-07 11:25:14 +0100

    added service interface

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,59 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract(
+        SessionMode = SessionMode.Required,
+        CallbackContract = typeof(IAutoReservationService))]
     public interface IAutoReservationService
     {
+        [OperationContract(IsOneWay = true)]
+        void GetAllAutos();
+
+        [OperationContract(IsOneWay = true)]
+        void GetAuto(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void AddAuto(AutoDto auto);
+
+        [OperationContract(IsOneWay = true)]
+        void UpdateAuto(AutoDto auto);
+
+        [OperationContract(IsOneWay = true)]
+        void RemoveAuto(AutoDto auto);
+
+        [OperationContract(IsOneWay = true)]
+        void GetAllReservationen();
+
+        [OperationContract(IsOneWay = true)]
+        void GetReservation(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void AddReservation(ReservationDto reservation);
+
+        [OperationContract(IsOneWay = true)]
+        void UpdateReservation(ReservationDto reservation);
+
+        [OperationContract(IsOneWay = true)]
+        void RemoveReservation(ReservationDto reservation);
+
+        [OperationContract(IsOneWay = true)]
+        void GetAllKunden();
+
+        [OperationContract(IsOneWay = true)]
+        void GetKunde(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void AddKunde(KundeDto kunde);
+
+        [OperationContract(IsOneWay = true)]
+        void UpdateKunde(KundeDto kunde);
+
+        [OperationContract(IsOneWay = true)]
+        void RemoveKunde(KundeDto kunde);
+
+        [OperationContract(IsOneWay = true)]
+        void IsAutoAvailable(ReservationDto kunde);
     }
 }
