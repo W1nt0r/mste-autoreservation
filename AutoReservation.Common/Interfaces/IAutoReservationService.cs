08c810b5e33834b21f4ff83ed59e585ec5a38058
Supernaibaf <reiem_naibaf@hotmail.com>
2017-12-07 11:11:07 +0100

    Implemented Callback-Interface

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,27 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.ObjectModel;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
     public interface IAutoReservationService
     {
+        [OperationContract(IsOneWay = true)]
+        void SendAuto(AutoDto auto);
+
+        [OperationContract(IsOneWay = true)]
+        void SendAllAutos(Collection<AutoDto> autos);
+
+        [OperationContract(IsOneWay = true)]
+        void SendKunde(KundeDto kunde);
+
+        [OperationContract(IsOneWay = true)]
+        void SendAllKunden(Collection<KundeDto> kunden);
+
+        [OperationContract(IsOneWay = true)]
+        void SendReservation(ReservationDto reservation);
+
+        [OperationContract(IsOneWay = true)]
+        void SendAllReservationen(Collection<ReservationDto> reservationen);
     }
 }
