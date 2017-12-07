827599b522538ec6a1aaa953d5f734a20cf9e5a5
w1nt0r <j4n.winter@gmail.com>
2017-12-07 12:02:57 +0100

    added faults + updated fault contracts in the service interface

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using AutoReservation.Common.DataTransferObjects.Faults;
 using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
@@ -12,45 +13,71 @@ namespace AutoReservation.Common.Interfaces
         void GetAllAutos();
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(EntityNotFoundFault))]
         void GetAuto(int id);
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(DatabaseChangeFault))]
         void AddAuto(AutoDto auto);
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(EntityNotFoundFault))]
+        [FaultContract(typeof(DatabaseChangeFault))]
+        [FaultContract(typeof(OptimisticConcurrencyFault))]
         void UpdateAuto(AutoDto auto);
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(EntityNotFoundFault))]
+        [FaultContract(typeof(DatabaseChangeFault))]
+        [FaultContract(typeof(OptimisticConcurrencyFault))]
         void RemoveAuto(AutoDto auto);
 
         [OperationContract(IsOneWay = true)]
         void GetAllReservationen();
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(EntityNotFoundFault))]
         void GetReservation(int id);
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(DatabaseChangeFault))]
         void AddReservation(ReservationDto reservation);
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(EntityNotFoundFault))]
+        [FaultContract(typeof(DatabaseChangeFault))]
+        [FaultContract(typeof(OptimisticConcurrencyFault))]
+        [FaultContract(typeof(InvalidDateRangeFault))]
+        [FaultContract(typeof(AutoUnavailableFault))]
         void UpdateReservation(ReservationDto reservation);
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(EntityNotFoundFault))]
+        [FaultContract(typeof(DatabaseChangeFault))]
+        [FaultContract(typeof(OptimisticConcurrencyFault))]
         void RemoveReservation(ReservationDto reservation);
 
         [OperationContract(IsOneWay = true)]
         void GetAllKunden();
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(EntityNotFoundFault))]
         void GetKunde(int id);
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(DatabaseChangeFault))]
         void AddKunde(KundeDto kunde);
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(EntityNotFoundFault))]
+        [FaultContract(typeof(DatabaseChangeFault))]
+        [FaultContract(typeof(OptimisticConcurrencyFault))]
         void UpdateKunde(KundeDto kunde);
 
         [OperationContract(IsOneWay = true)]
+        [FaultContract(typeof(EntityNotFoundFault))]
+        [FaultContract(typeof(DatabaseChangeFault))]
+        [FaultContract(typeof(OptimisticConcurrencyFault))]
         void RemoveKunde(KundeDto kunde);
 
         [OperationContract(IsOneWay = true)]
